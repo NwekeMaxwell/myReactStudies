3e65b93e12fb9f8baccc737bbeb55ab899d01bdc
NwekeMaxwell <nwekemaxwell37@gmail.com>
2023-04-23 11:43:48 +0100

    fetch API

---
+++ fetchAPI/fetchAPI.jsx
@@ -0,0 +1,32 @@
+// how to fetch data from API
+// learn fetch function and Axios library
+// we build an simple excuse generator app for different situations like for office, family or party
+
+import "./App.css";
+import Axios from "axios";
+import { useEffect, useState } from "react";
+
+function App() {
+  const [generatedExcuse, setGeneratedExcuse] = useState("");
+
+  const fetchExcuse = (excuse) => {
+    Axios.get(`https://excuser-three.vercel.app/v1/excuse/${excuse}/`).then(
+      (res) => {
+        setGeneratedExcuse(res.data[0].excuse);
+      }
+    );
+  };
+
+  return (
+    <div className="App">
+      <h1> Generate An Excuse </h1>
+      <button onClick={() => fetchExcuse("party")}> Party</button>
+      <button onClick={() => fetchExcuse("family")}> Family</button>
+      <button onClick={() => fetchExcuse("office")}> Office </button>
+
+      <p> {generatedExcuse} </p>
+    </div>
+  );
+}
+
+export default App;
