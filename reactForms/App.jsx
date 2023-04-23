71b51ec4865eaf5630a777b7b5cffbea8ccf47f9
NwekeMaxwell <nwekemaxwell37@gmail.com>
2023-04-23 12:36:09 +0100

    react forms

---
+++ reactForms/App.jsx
@@ -0,0 +1,14 @@
+//understand the functionalities of react form
+
+import "./App.css";
+import { Form } from "./components/Form";
+
+function App() {
+  return (
+    <div className="App">
+      <Form />
+    </div>
+  );
+}
+
+export default App;
